e65f0f311af1fc1ac5f86e194d7ca13ed3bdd6b5
Santiago <calgaro.santiago@gmail.com>
2021-11-13 11:44:18 -0300

    updates

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,8 +1,16 @@
 package vista;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //Creating object of LoginFrame class and setting some of its properties
+        LoginFrame frame=new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10,10,370,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 }
